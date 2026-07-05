0f1ea2b7d713a1779598f4aacdca110a8d4df9c6
Austin Gebauer <gebauer.austin@gmail.com>
2026-07-05 11:38:57 -0700

    polish provision script

--- provisioners/ubuntu.sh
+++ provisioners/ubuntu.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-echo "# Start: generate ssh key for GitHub authentication"
+set -euo pipefail
+
+function print_header() {
+    echo "============================================="
+    echo "$1"
+    echo "============================================="
+}
+
+print_header "Install packages"
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y git neovim
+echo ""
+
+print_header "Set peripherals settings"
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 15
+gsettings get org.gnome.desktop.peripherals.keyboard repeat-interval
+gsettings set org.gnome.desktop.peripherals.keyboard delay 200
+gsettings get org.gnome.desktop.peripherals.keyboard delay
+echo ""
+
+print_header "Generate ssh key for GitHub authentication"
 KEY_PATH="$HOME/.ssh/github_id_ed25519"
 mkdir -p "$HOME/.ssh"
 chmod 700 "$HOME/.ssh"
@@ -14,9 +34,8 @@ if [ ! -f "$KEY_PATH" ]; then
 else
     echo "SSH key already exists at $KEY_PATH. Skipping generation."
 fi
-
-echo "## Action: add public key to GitHub $(cat ${KEY_PATH}.pub)"
-echo "# Finish: generate ssh key for GitHub authentication"
+echo "ACTION: add public key to GitHub $(cat ${KEY_PATH}.pub)"
+echo ""
 
 # Install Go tools
 #go install github.com/go-delve/delve/cmd/dlv@latest
