6654bce53873697a33932efaea4af87444f19b56
Austin Gebauer <gebauer.austin@gmail.com>
2026-07-05 11:16:28 -0700

    start of ubuntu provisioner

--- provisioners/ubuntu.sh
+++ provisioners/ubuntu.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# Generate a new ssh key for GitHub authentication
+KEY_PATH="$HOME/.ssh/github_id_ed25519"
+mkdir -p "$HOME/.ssh"
+chmod 700 "$HOME/.ssh"
+if [ ! -f "$KEY_PATH" ]; then
+    echo "Key not found. Generating ED25519 SSH key..."
+    ssh-keygen -t ed25519 -C "gebauer.austin@gmail.com" -f "$KEY_PATH" -N "" -q
+    chmod 600 "$KEY_PATH"
+    chmod 644 "${KEY_PATH}.pub"
+    
+    echo "SSH key successfully generated."
+else
+    echo "SSH key already exists at $KEY_PATH. Skipping generation."
+fi
 
 # Install Go tools
-go install github.com/go-delve/delve/cmd/dlv@latest
-go install honnef.co/go/tools/cmd/staticcheck@latest
-go install golang.org/x/tools/cmd/godoc@latest
-go install golang.org/x/tools/cmd/stress@latest
+#go install github.com/go-delve/delve/cmd/dlv@latest
+#go install honnef.co/go/tools/cmd/staticcheck@latest
+#go install golang.org/x/tools/cmd/godoc@latest
+#go install golang.org/x/tools/cmd/stress@latest
